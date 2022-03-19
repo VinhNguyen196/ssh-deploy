f25f1395570c6792b8e0d66d14355f617e4e1d0c
Vinh Nguyen <vinhnquoc196@gmail.com>
2022-03-19 17:48:07 +0700

    update

--- app/script/clean.sh
+++ app/script/clean.sh
@@ -6,16 +6,10 @@ echo $TEMP_STR
 if [ "$TEMP_STR" != "" ]
 then
  TEMP_STR=$( echo ${TEMP_STR} | sed 's/^ *//g')
-
  TEMP_STR=${TEMP_STR/ /:}
-
  TEMP_STR=${TEMP_STR:0:`expr index "$TEMP_STR" " "`}
-
  sudo sed -i 's/export DOCKER_TAG=/export DOCKER_TAG=${TEMP_STR:`expr index "$TEMP_STR" ":"`}/g' ~/.bashrc
-
  mkdir -p ./deploy && cd deploy
-
  docker compose down
-
  docker image rm $DOCKER_IMAGE:$DOCKER_TAG
 fi
